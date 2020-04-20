776876f35acdf0f96231380220ccf7dfa2669a11
Adam Scarlat <adam.scarlat@philips.com>
2020-04-20 09:10:13 -0400

    added builder service - no-cache

---
+++ todobackend/src/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup (
+    name                    = "todobackend",
+    version                 = "0.1.0",
+    description             = "Todobackend Django REST service",
+    packages                = find_packages(),
+    include_package_data    = True,
+    script                  = ["manage.py"],
+    install_requires        = [ "asgiref==3.2.7",
+                                "Django==3.0.5",
+                                "django-cors-headers==3.2.1",
+                                "djangorestframework==3.11.0",
+                                "mysqlclient==1.4.6",
+                                "pytz==2019.3",
+                                "sqlparse==0.3.1"],
+    extras_require          = {
+                                "test": [
+                                    "colorama==0.4.3",
+                                    "coverage==5.1",
+                                    "django-nose==1.4.6",
+                                    "nose==1.3.7",
+                                    "pinocchio==0.4.2"
+                                ]
+                              }
+)

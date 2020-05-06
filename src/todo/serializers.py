fabc685957d089d0a9964918319872a06b4546ec
Adam Scarlat <adam.scarlat@philips.com>
2020-05-06 15:40:59 -0400

    testing CD pipeline

--- src/todo/serializers.py
+++ src/todo/serializers.py
@@ -5,5 +5,5 @@ class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.ReadOnlyField()
     class Meta:
         model = TodoItem
-        fields = ('url', 'title', 'completed', 'order')
+        fields = ('url', 'title', 'completed')
         
